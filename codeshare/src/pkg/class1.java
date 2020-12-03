5e9609b11b68c3b0ed5580b975c36905efe2da01
lalit <lalit@10.0.0.102>
2020-12-02 23:59:58 -0600

    pushing code first time

---
+++ codeshare/src/pkg/class1.java
@@ -0,0 +1,13 @@
+package pkg;
+
+public class class1 
+{
+public void abc()
+{
+	System.out.println("class1");
+}
+public static void main(String[] args) {
+	class1 c= new class1();
+	c.abc();
+}
+}
